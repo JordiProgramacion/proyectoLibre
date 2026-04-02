279b6d2998cf5a830a243f21469dec1df0835b27
Jordi <jmart3@insdanielblanxart.cat>
2026-04-03 00:39:13 +0200

    logica del main

--- src/main/kotlin/app/main.kt
+++ src/main/kotlin/app/main.kt
@@ -2,21 +2,31 @@ package app
 
 import core.Foro
 import utils.Utils
+import models.Usuario
+import models.UsuarioInvitado
 
 fun registroUsuario(foro: Foro) {
     print("Bienvenido al panel de registro de usuarios, por favor ingrese su nombre: ")
-    val nombre = readln().replaceFirstChar {it.uppercase()}
+    val nombre = readln().trim().replaceFirstChar {it.uppercase()}
     print("Ahora ingrese una contraseña segura: ")
     // Implementar metodo para que no se vea la contraseña en el terminal
     val contrasenaNuevoUsuario = readln()
     foro.registrarUsuario(nombre, contrasenaNuevoUsuario)
 }
-fun inicioSesion(foro: Foro): Boolean {
+fun inicioSesion(foro: Foro): Usuario? {
     print("Bienvenido al panel de inicio de sesión, por favor ingrese su id: ")
-    val idUsuario = readln().toIntOrNull() ?: 10000000
+    val idUsuario = readln().toIntOrNull() ?: -1
     print("Ahora introduzca su contraseña: ")
     val contraUsuario = readln()
-    return foro.iniciarSesion(idUsuario, contraUsuario)
+
+    val inicioSesion = foro.iniciarSesion(idUsuario, contraUsuario)
+    val usuario = foro.usuarios.find { it.id == idUsuario && it.contrasena == contraUsuario }
+    return if (inicioSesion) {
+        usuario
+    } else {
+        null
+    }
+    // return foro.usuarios.find { it.id == id && it.contrasena == pass} (esto es una mejor opción, hay que mirarlo)
 }
 fun menuMenuDeInicio() {
     print("""
@@ -30,7 +40,7 @@ fun menuMenuDeInicio() {
             ------------------------
         """.trimIndent())
 }
-fun menuDeInicio(foro: Foro): Pair<Boolean, Int> {
+fun menuDeInicio(foro: Foro): Pair<Usuario?, Int> {
     do {
         menuMenuDeInicio()
         print("\nEscoja la opció que quiere hacer: ")
@@ -38,17 +48,18 @@ fun menuDeInicio(foro: Foro): Pair<Boolean, Int> {
         when (opcio) {
             "1" -> {
                 // Iniciar sesión
-                val estado = inicioSesion(foro)
-                return Pair(estado, 1)
+                val usuario = inicioSesion(foro)
+                return if (usuario != null) Pair(usuario, 1) else Pair(null, 1)
+                //return Pair(estado, 1)
             }
             "2" -> {
                 // Registro usuarios
                 registroUsuario(foro)
-                return Pair(false, 1)
+                return Pair(null, 1)
             }
             "84938493" -> {
                 sesionInvitado() // Pendiente de arreglar, sesión invitado
-                return Pair(false, 1)
+                return Pair(null, 1) // Hay que cambiar lo de que retorne null
             }
             "0" -> {
                 println("Saliendo del programa...")
@@ -56,7 +67,7 @@ fun menuDeInicio(foro: Foro): Pair<Boolean, Int> {
             else -> println("Escribe una opción valida (1-4).")
         }
     } while (opcio != "0")
-    return Pair(false, 0)
+    return Pair(null, 0)
 }
 fun menuMenuSesionIniciada() {
     println("""
@@ -72,30 +83,34 @@ fun menuMenuSesionIniciada() {
         ------------------------------------
     """.trimIndent())
 }
-fun hacerPregunta() {
-    // hacer pregunta
+fun verPreguntas(foro: Foro) {
+
 }
-fun menuSesionIniciada(foro: Foro): Boolean {
+fun hacerPregunta(foro: Foro) {
+
+}
+fun menuSesionIniciada(foro: Foro, usuario: Usuario): Boolean {
     do {
         menuMenuSesionIniciada()
         print("\nEscoja la opció que quiere hacer: ")
         val opcio = readln()
         when (opcio) {
             "1" -> {
-                return true
+                verPreguntas(foro)
+
             }
             "2" -> {
-                return true
+                hacerPregunta(foro)
+
             }
             "3" -> {
-                return true
+
             }
             "4" -> {
-                return true
+
             }
             "5" -> {
                 println("Ahora mismo hay un total de: ${Utils.contarPreguntas(foro.preguntas)}")
-                return true
             }
             "0" -> {
                 println("Cerrando sesión...")
@@ -110,15 +125,19 @@ fun sesionInvitado() {
 }
 fun main() {
     val foro = Foro()
-    var usuarioLog = false
+    var usuarioActual: Usuario? = null
+    //var usuarioLog = false
     var seguirPrograma = 0
     do {
-        if (!usuarioLog) {
-            val (estado, programa) = menuDeInicio(foro)
-            usuarioLog = estado
+        if (usuarioActual == null) {
+            val (usuario, programa) = menuDeInicio(foro)
+            usuarioActual = usuario
             seguirPrograma = programa
         } else {
-            usuarioLog = menuSesionIniciada(foro)
+            val sigueLogueado = menuSesionIniciada(foro, usuarioActual)
+            if (!sigueLogueado) {
+                usuarioActual = null
+            }
         }
     } while (seguirPrograma != 0)
 }
